ca0d0f71626595a5fb57213fde40436535e9982e
musesum <musesum@gmail.com>
2023-06-19 11:07:04 +0800

    add .

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Goals
     synchronize performance with static streams
         spotify, apple music, etc
         extend to live broadcast of major events
+Notes
+    in flo.par, the order of expr is imporant
+    
+    expr   ≈ (scalar | exprOp | name | quote | comment)+
+    in  exprBad1  ≈ (exprOp |scalar | name | quote | comment)+
+    
+        a(%2~1) won't part `~1` as the `%` is interpreted as an exprOp
+        
+    in  exprBad2  ≈ ( name | exprOp |scalar |quote | comment)+
+    
+        b(x in 0…2) will interpret in as a name instead of an exprOp
--- Sky/SkyPipeline+ext.swift
+++ Sky/SkyPipeline+ext.swift
@@ -13,29 +13,18 @@ extension SkyPipeline {
         guard let compute = shader.findPath("compute") else { return err(flo,"compute")}
         guard let render  = shader.findPath("render" ) else { return err(flo,"render" )}
 
-        for flo in cell.children {
-            if let node = initNodeName(flo.name) {
-                makeNode(node, flo)
-            }
-        }
-        for flo in compute.children {
-            if let node = initNodeName(flo.name) {
-                makeNode(node, flo)
-            }
-        }
-        for flo in render.children {
-            if let node = initNodeName(flo.name) {
-                makeNode(node, flo)
-            }
-        }
-    }
-    func makeNode(_ node: MetNode,
-                  _ flo: Flo) {
+        for child in cell.children    { makeMetNode(child) }
+        for child in compute.children { makeMetNode(child) }
+        for child in render.children  { makeMetNode(child) }
 
-        updateBuffer(node, flo)
+        func makeMetNode(_ flo: Flo) {
 
-        for child in flo.children {
-            switch child.name {
+            guard let node = initNodeName(flo.name) else { return }
+
+            updateBuffer(node, flo)
+
+            for child in flo.children {
+                switch child.name {
                 case "on"     : addMtlNodeOn(node, child)
                 case "loops"  : node.loops = max(1,child.int)
                 case "flip"   : break // ignore
@@ -46,75 +35,76 @@ extension SkyPipeline {
                 case "bits"   : updateChildBuffer(node, child)
                 case "gravity": updateChildBuffer(node, child)
                 default       : err(flo, "name:\(child.name)")
-            }
-        }
-    }
-    func updateBuffer(_ node: MetNode,
-                      _ flo: Flo) {
-
-        func updateFloats(_ flo: Flo, _: Visitor = Visitor(0)) {
-            if let exprs = flo.exprs {
-                let nums = exprs.getValNums()
-                node.updateBuffer(flo.name, nums)
-            }
-        }
-        flo.addClosure(updateFloats)
-        updateFloats(flo)
-    }
-
-    func anyInt(_ any: Any?) -> Int {
-        if let v = any as? FloValScalar {
-            return Int(v.twe)
-        } else {
-            print("*** SkyMetal::anyInt: unknown: \(any.debugDescription)")
-            return 0
-        }
-    }
-    func addMtlNodeOn(_ node: MetNode,
-                      _ flo: Flo) {
-
-        node.isOn = flo.bool
-        flo.addClosure(nodeOn)
-
-        func nodeOn(_ flo: Flo, _: Visitor) {
-
-            if let name = flo.parent?.name,
-               let node = nodeNamed[name] {
-
-                let isOn = flo.bool
-
-                setMtlNode(node, isOn)
-
-                if name == "camera" {
-                    MetCamera.shared.setCameraOn(isOn)
-                    _ = CameraFlip.shared // setup singleton
-                    if let mixNode = nodeNamed["camix"] {
-                        setMtlNode(mixNode, isOn)
-                    }
                 }
             }
         }
-        func setMtlNode(_ node: MetNode,_ isOn: Bool) {
-            node.setMetalNodeOn(isOn) {
-                self.swap(inNode: node)
-            }
-        }
-    }
-    func updateChildBuffer(_ node: MetNode,
-                           _ child: Flo) {
-        child.addClosure { child,_ in
-            updateParentFloats(child)
-        }
-        updateParentFloats(child)
+        func updateBuffer(_ node: MetNode,
+                          _ flo: Flo) {
 
-        func updateParentFloats(_ child: Flo) {
-            if  let exprs = child.exprs {
-                let nums = exprs.getValNums()
-                node.updateBuffer(child.name, nums)
+            func updateFloats(_ flo: Flo, _: Visitor = Visitor(0)) {
+                if let exprs = flo.exprs {
+                    let nums = exprs.getValNums()
+                    node.updateBuffer(flo.name, nums)
+                }
+            }
+            flo.addClosure(updateFloats)
+            updateFloats(flo)
+        }
+
+        func anyInt(_ any: Any?) -> Int {
+            if let v = any as? FloValScalar {
+                return Int(v.twe)
+            } else {
+                print("*** SkyMetal::anyInt: unknown: \(any.debugDescription)")
+                return 0
             }
         }
-    }
-    func err(_ flo: Flo, _ item: String) {
-        print("🚫 makeShader couldn't find \(item) in \(flo.name)")
+        func addMtlNodeOn(_ node: MetNode,
+                          _ flo: Flo) {
+
+            node.isOn = flo.bool
+            flo.addClosure(nodeOn)
+
+            func nodeOn(_ flo: Flo, _: Visitor) {
+
+                if let name = flo.parent?.name,
+                   let node = nodeNamed[name] {
+
+                    let isOn = flo.bool
+
+                    setMtlNode(node, isOn)
+
+                    if name == "camera" {
+                        MetCamera.shared.setCameraOn(isOn)
+                        _ = CameraFlip.shared // setup singleton
+                        if let mixNode = nodeNamed["camix"] {
+                            setMtlNode(mixNode, isOn)
+                        }
+                    }
+                }
+            }
+            func setMtlNode(_ node: MetNode,_ isOn: Bool) {
+                node.setMetalNodeOn(isOn) {
+                    self.swap(inNode: node)
+                }
+            }
+        }
+        func updateChildBuffer(_ node: MetNode,
+                               _ child: Flo) {
+            child.addClosure { child,_ in
+                updateParentFloats(child)
+            }
+            updateParentFloats(child)
+
+            func updateParentFloats(_ child: Flo) {
+                if  let exprs = child.exprs {
+                    let nums = exprs.getValNums()
+                    node.updateBuffer(child.name, nums)
+                }
+            }
+        }
+        func err(_ flo: Flo, _ item: String) {
+            print("🚫 makeShader couldn't find \(item) in \(flo.name)")
+        }
     }
 }
